1fd47d09acb6505fed2b0e9c23966dec0ecf3906
Trung <namtrung18101996@gmail.com>
2018-02-13 15:20:23 +0700

    Update2

--- typescript/dist/lesson16Forof.js
+++ typescript/dist/lesson16Forof.js
@@ -1,3 +1,4 @@
+//for
 var colorNames = ["red", "green", "Yellow", "Purple"];
 for (var i in colorNames) {
     console.log(colorNames[i]);
@@ -7,4 +8,10 @@ for (var _i = 0, colorNames_1 = colorNames; _i < colorNames_1.length; _i++) {
     var item = colorNames_1[_i];
     console.log(item);
 }
+//vd2
+var ten = 'Trung';
+for (var _a = 0, ten_1 = ten; _a < ten_1.length; _a++) {
+    var c_1 = ten_1[_a];
+    console.log(c_1); //of T,R,U,N,G
+} //in 0,1,2,3,4
 //# sourceMappingURL=lesson16Forof.js.map
--- typescript/lesson16Forof.ts
+++ typescript/lesson16Forof.ts
@@ -1,3 +1,4 @@
+//for
 var colorNames  = ["red","green","Yellow","Purple"]
 for(let i in colorNames){
     console.log(colorNames[i]);
@@ -7,3 +8,10 @@ for(let i in colorNames){
 for(let item of colorNames){
     console.log(item);
 }
+
+//vd2
+
+var ten = 'Trung';
+for(let c of ten){
+    console.log(c);//of T,R,U,N,G
+}                  //in 0,1,2,3,4
